8e12cbeb25dd9aaf75b2a6f74b622d1b0d4dbdfd
Ionela Tataru <ionela.mda@gmail.com>
2024-06-13 01:26:18 +0200

    - Use `read_csv2`(for the `;` separator);

--- 02_r_scripts/download-the-chars-imgs.R
+++ 02_r_scripts/download-the-chars-imgs.R
@@ -1,13 +1,13 @@
 ###
-## Download the images of the characters from the URLs in the shortversioncharacters.csv file:
-## The images will be saved in the "characters-images" folder of the 01_tidy_data folder
+## Download the images of the characters from the URLs in the shortversioncharacters.csv file.
+## The images will be saved in the "characters-images" folder of the 01_tidy_data folder.
 ###
 
 # Load the necessary libraries
 library(tidyverse)
 
 # Load the data
-shortversioncharacters <- read_csv("00_raw_data/hp-scripts-kaggle-dataset/shortversioncharacters.csv")
+shortversioncharacters <- read_csv2("00_raw_data/hp-scripts-kaggle-dataset/shortversioncharacters.csv")
 
 # Create the folder (if not already exists) to save the images
 dir.create("01_tidy_data/characters-images", showWarnings = FALSE)
