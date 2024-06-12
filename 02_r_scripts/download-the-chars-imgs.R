660b9626e1594454d002c37fca56b3b5bcc06c71
Ionela Tataru <ionela.mda@gmail.com>
2024-06-12 23:25:03 +0200

    - Add an R script to download the images with the characters from `00_raw_data/hp-scripts-kaggle-dataset/shortversioncharacters.csv:image;

---
+++ 02_r_scripts/download-the-chars-imgs.R
@@ -0,0 +1,20 @@
+
+# Download the images of the characters from the URLs in the shortversioncharacters.csv file:
+# The images will be saved in the "imgs" folder of the 01_tidy_data folder
+
+# Load the necessary libraries
+library(tidyverse)
+
+# Load the data
+shortversioncharacters <- read_csv("00_raw_data/hp-scripts-kaggle-dataset/shortversioncharacters.csv")
+
+# Create the folder (if not already exists) to save the images
+dir.create("01_tidy_data/imgs", showWarnings = FALSE)
+
+# Download the images
+for (i in 1:nrow(shortversioncharacters)) {
+  download.file(
+      shortversioncharacters$image[i],
+      destfile = paste0("01_tidy_data/imgs/", shortversioncharacters$name[i], ".jpg")
+      )
+}
