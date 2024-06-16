130ac6aee6c3f2f54f58cc287724d65995ab1196
Flara27 <larads2013@gmail.com>
2024-06-16 19:32:02 +0200

    Graphs and their data saved

--- mentionetwork.r
+++ mentionetwork.r
@@ -13,6 +13,10 @@ file_path <- ("HP_Scripts_dataset/datasets/combined.csv")
 hpscripts <- read_csv(file_path, locale = locale(encoding = "UTF-16"))
 characters <-unique(hpscripts$character)
 characters
+filtered_characters <- characters[grepl("^[A-Za-z]+ [A-Za-z]+$", characters)]
+filtered_characters <- c(filtered_characters, "Voldemort", "Dobby")
+characters=filtered_characters
+characters
 
 ####
 character_mentions <- list()
@@ -47,6 +51,7 @@ character_mentions_df <- do.call(rbind.data.frame, character_mentions)
 rownames(character_mentions_df) <- NULL
 character_mentions_df <- character_mentions_df[order(-character_mentions_df$Frequency), ]
 head(character_mentions_df)
+write.csv(character_mentions_df, "HP_Scripts_dataset/img/character_mentions.csv", row.names = FALSE)
 
 ####
 directed_edges_df <- data.frame(From = character(), To = character(), Weight = numeric(), stringsAsFactors = FALSE)
@@ -65,7 +70,6 @@ for (i in 1:nrow(character_mentions_df)){
 }
 directed_edges_df
 
-
 ####
 directed_edges_df <- directed_edges_df %>%
   filter(From != "" & To != "")
@@ -74,7 +78,6 @@ directed_edges_df <- directed_edges_df %>%
   summarise(Weight = sum(Weight)) %>%
   ungroup()
 
-####
 g <- graph_from_data_frame(directed_edges_df, directed = TRUE)
 E(g)$weight <- directed_edges_df$Weight
 
@@ -90,24 +93,35 @@ text(x = rep(0.5, nrow(head(directed_edges_df))), y = label_y, labels = relation
 text(x=0.5, y=0.8,"Instances of edges",  cex = 1.5)
 dev.off()
 
-#### chaotic
-data <- toVisNetworkData(g)
-visNetwork(nodes = data$nodes, edges = data$edges) %>%
-  visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE) %>%
-  visInteraction(navigationButtons = TRUE) %>%
-  visPhysics(stabilization = TRUE)  # Disable physics to prevent bouncing
 
 ####
 g <- igraph::simplify(g, remove.multiple = TRUE, remove.loops = FALSE, 
                  edge.attr.comb=c(weight="sum"))              
-E(g)$color <- ifelse(E(g)$weight > 200, "purple",
-               ifelse(E(g)$weight > 100, "blue",
-               ifelse(E(g)$weight > 50, "green",
-               ifelse(E(g)$weight > 20, "yellow", "red"))))
-set.seed(5) # Plot the graph (weighted)  
+E(g)$color <- ifelse(E(g)$weight > 100, "purple",
+               ifelse(E(g)$weight > 50, "red",
+               ifelse(E(g)$weight > 30, "yellow",
+               ifelse(E(g)$weight > 20, "blue", "green"))))
+png(paste0("HP_Scripts_dataset/img/mentiongraph.png"), width = 800, height = 800)
+set.seed(2.9) # Plot the graph (weighted)  
 par(mar = c(0,0,0,0))
 plot(g, vertex.shape="circle",vertex.label.color="black", edge.arrow.size = 0.3, vertex.size = 3,
-     vertex.label.cex=1,vertex.label.dist=1, edge.curved=0.5,layout=layout_with_fr(g)*2,rescale=F,edge.color = E(g)$color,layout.center = TRUE)
+     vertex.label.cex=1,vertex.label.dist=1, edge.curved=0.5,layout=layout_with_fr(g)*0.9,rescale=F,edge.color = E(g)$color,layout.center = TRUE)
 # Add a legend
-legend("bottomright", legend = c("0-10", "20-50", "50-100", "100-200", "200+"),
-       col = c("red", "yellow", "green", "blue", "purple"), lty = 2, cex = 1.5)
+legend("bottomright", legend = c("0-10", "10-20", "20-30", "30-50", "100+"),
+       col = c("green", "blue", "yellow", "red", "purple"), lwd = 2, cex = 1,
+       title = "Edge Weight Range")
+dev.off()
+
+
+#### chaotic
+data <- toVisNetworkData(g)
+# Plot the network with customized edge colors
+visNetwork(nodes = data$nodes, edges = data$edges) %>%
+  visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE) %>%
+  visInteraction(navigationButtons = TRUE)
+  visPhysics(enabled = T)
+data
+write.csv(data$nodes, "HP_Scripts_dataset/img/character_mentions_nodes.csv", row.names = FALSE)
+edges_df <- data$edges[, c("from", "to", "weight", "color")]
+data$edges$Weight <- NULL
+write.csv(data$edges, "HP_Scripts_dataset/img/character_mentions_edges.csv", row.names = FALSE)
